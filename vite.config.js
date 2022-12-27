ea1fd43a2a01c6b8bfda808a29cb99d01cc59073
offbeat-dev <offbeat.it.all@gmail.com>
2022-12-27 11:15:35 -0500

    :construction: -- add and test plugins

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,32 @@ import { resolve } from "path";
 import { defineConfig } from "vituum";
 import handlebars from "@vituum/handlebars";
 
+const vitePluginExample = () => ({
+  name: "output-plugin-stats",
+  configResolved(config) {
+    const plugins = config.plugins.map((plugin) => plugin.name);
+    console.log(`Your project has ${plugins.length} Vite plugins.`);
+    console.table(plugins);
+  },
+});
+
 const config = {
-  ...defineConfig({ integrations: [handlebars()] }),
+  ...defineConfig({
+    integrations: [handlebars({})],
+  }),
   vituum: {
     ...defineConfig().vituum,
-    input: [...defineConfig().vituum.input, "./src/html/**/*.html"],
+    input: [...defineConfig().vituum.input, "./src/html/**/*.hbs"],
     middleware: {
       ...defineConfig().vituum.middleware,
       viewsDir: resolve(__dirname, "./src/html"),
     },
   },
+  plugins: [...defineConfig().plugins, vitePluginExample()],
 };
 
 console.log("CONFIG =========", config);
 console.log("PATH =========", resolve(__dirname));
+console.log(handlebars());
 
 export default config;
